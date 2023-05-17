ff45defe0980adf561f07bc9f3b06368c4cd6d1a
21753059-Mukaromah <70304033+roma84011@users.noreply.github.com>
2023-05-17 11:51:51 +0700

    commit

--- server_side/kas_masuk/hapus_kas_masuk.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once '../../koneksi/conn.php';
-$id = $conn->real_escape_string($_GET['id']);
-
-$sql=$conn->query("DELETE FROM kas_masuk WHERE id='$id' ");
-if ($sql) {
-    echo json_encode(array("status" => TRUE));
-}
-?>
